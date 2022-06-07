d907c763a1ab763898cae3e34adece6e86336214
KostyaAnd <kostya.and83@gmail.com>
2022-06-07 11:43:45 +0300

    homework2

---
+++ homework2/go.mod
@@ -0,0 +1,3 @@
+module /home/konstantin/go/srс/golang-two/homework2
+
+go 1.18
---
+++ homework2/package2/divide_doc.go
@@ -0,0 +1,32 @@
+// Package divide_doc implements functions to demonstrate godoc features
+// The divide_Doc returns some result from the division of two integers
+// If one of the numbers is zero, an error will be occurred
+// if the result of the division is zero, then a message will be displayed
+
+package divide_Doc
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	safeDivide(a, b)
+}
+
+func safeDivide(a, b int) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			if fmt.Sprintf("%v", err) == "runtime error: integer divide by zero" {
+				log.Println("Введите делимое не нулевое значение")
+				return
+			}
+			log.Printf("Panic: %v", err)
+		}
+	}()
+
+	fmt.Println("Результат: ", a/b)
+}
